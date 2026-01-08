c969f12e89d5fd1a1c562423faa0004266e4fa81
Ryouse1 <ryouseikoseki1114@gmail.com>
2026-01-08 10:51:50 +0900

    Implement TranslatedStdout for output translation
    Refactor stdout handling to translate output text.

---
+++ console/stdout_hook.py
@@ -0,0 +1,11 @@
+import sys
+from .translator import translate
+
+class TranslatedStdout:
+    def write(self, text):
+        sys.__stdout__.write(translate(text))
+    def flush(self):
+        pass
+
+def enable():
+    sys.stdout = TranslatedStdout()
