772301d7e3df002af2f58b8956996af1fb70227c
Ryouse1 <ryouseikoseki1114@gmail.com>
2026-01-08 11:20:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="console_translator",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=[
-        "googletrans==4.0.0-rc1"
-    ]
-)
