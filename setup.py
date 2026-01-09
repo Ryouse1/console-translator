60d763db26cfcf3eac345abae636e8eb41658664
Ryouse1 <ryouseikoseki1114@gmail.com>
2026-01-09 10:28:21 +0900

    Create setup.py for console-translator package
    Add setup configuration for console-translator package

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="console-translator",
+    version="0.1.0",
+    description="Translate console output and errors into Japanese",
+    author="Ryouse1",
+    url="https://github.com/Ryouse1/console-translator",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "deep-translator",  # 非公式Google翻訳（安定枠）
+    ],
+)
